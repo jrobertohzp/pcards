bb50047551e14603b1da8abd27b7021754ee3dfc
Rob <jrobertohzp@gmail.com>
2024-01-18 22:03:19 -0600

    images loaded and first component created

---
+++ src/components/Testimonial.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Testimonial() {
+  return (
+    <div className="testimonial-container">
+      <img
+        className="testimonial-image"
+        src="../images/berlioz.jpg"
+        alt="quote" />
+    </div>
+
+  );
+}
