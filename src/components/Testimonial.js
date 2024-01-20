b8efa13f1b618c89d227b7607f6c5c180a845462
Rob <jrobertohzp@gmail.com>
2024-01-19 22:53:11 -0600

    Testimonial texts created, unnecessary files deleted

--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -5,8 +5,13 @@ function Testimonial() {
     <div className="testimonial-container">
       <img
         className="testimonial-image"
-        src="../images/berlioz.jpg"
-        alt="quote" />
+        src={require("../images/berlioz.jpg")}
+        alt="Berlioz pic" />
+      <div className="testimonial-text-container">
+        <p className="testimonial-name">Berlioz in París</p>
+        <p className="testimonial-job">Software Engineer at Carrefour</p>
+        <p className="testimonial-text">"I have been working with the team for a few years now and it is always a pleasure. They have an excellent understanding of the business context and can therefore provide highly relevant candidates. They are very responsive and flexible, which is very valuable in the fast-paced world of recruitment. I highly recommend them!"</p>
+      </div>
     </div>
 
   );
