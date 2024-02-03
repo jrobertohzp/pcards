0bfd3f02f312e177b7ba3a26b51ddf479404459d
Rob <jrobertohzp@gmail.com>
2024-02-03 01:33:31 -0600

    props added, multiple elements

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import Testimonial from './components/Testimonial';
 
 function App() {
   return (
     <div className="App">
-
+      <div className="main-container">
+        <h1>This is what our students say about the Kodemia Bootcamp:</h1>
+        <Testimonial
+          name='Berlioz Bonfamille'
+          city='Paris'
+          image='berlioz'
+          job='Software Engineer'
+          company='Dell'
+          testimonial='I was a bit skeptical at first, but after the first week I was already building my own projects. I am now a full-time software engineer at Dell.'
+        />
+        <Testimonial
+          name='Marie Bonfamille'
+          city='Paris'
+          image='marie'
+          job='Web Developer'
+          company='Google'
+          testimonial='I had some experience with web development, but I was stuck. I joined the bootcamp and now I am a web developer at Google.'
+        />
+        <Testimonial
+          name='Toulouse Bonfamille'
+          city='Paris'
+          image='toulouse'
+          job='Data Scientist'
+          company='IBM'
+          testimonial='I had a background in mathematics and I wanted to learn how to apply it to the real world. Now I am a data scientist at IBM.'
+        />
+      </div>
     </div>
   );
 }
--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -1,18 +1,21 @@
 import React from "react";
+import "../stylesheets/Testimonial.css";
 
-function Testimonial() {
+function Testimonial(props) {
   return (
     <div className="testimonial-container">
       <img
         className="testimonial-image"
-        src={require("../images/berlioz.jpg")}
+        src={require(`../images/${props.image}.jpg`)}
         alt="Berlioz pic" />
       <div className="testimonial-text-container">
-        <p className="testimonial-name">Berlioz in París</p>
-        <p className="testimonial-job">Software Engineer at Carrefour</p>
-        <p className="testimonial-text">"I have been working with the team for a few years now and it is always a pleasure. They have an excellent understanding of the business context and can therefore provide highly relevant candidates. They are very responsive and flexible, which is very valuable in the fast-paced world of recruitment. I highly recommend them!"</p>
+        <p className="testimonial-name"><strong>{props.name}</strong>  in {props.city}</p>
+        <p className="testimonial-job">{props.job} at <strong>{props.company}</strong></p>
+        <p className="testimonial-text">"{props.testimonial}"</p>
       </div>
     </div>
 
   );
 }
+
+export default Testimonial;
